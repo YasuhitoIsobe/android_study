1ce5e6d6c61316e7618b5eb2e6f136d8afa5a0e2
Isobe <nsd.isobe@gmail.com>
2016-11-16 22:36:44 +0900

    imageViewTestを追加

---
+++ ImageViewTest/app/src/main/java/jp/co/n_sysdes/y_isobe/imageviewtest/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.co.n_sysdes.y_isobe.imageviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
